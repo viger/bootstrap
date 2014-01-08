29fb484105cdaba1bf77f7300d371f6f30c9e022
tom.chen <chenziwei@cd.tuan800.com>
2014-01-08 11:07:05 +0800

    init config

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,8 @@
-APP_ROOT = '/home/vhosts/tao800_fire'
+# You must replace 'app' to ur appname
+APP_ROOT = '/home/vhosts/app'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
-bind "unix:///var/run/puma_tao800_fire.sock"
+bind "unix:///var/run/puma_app.sock"
 daemonize true
 workers 1
 threads 0,16
